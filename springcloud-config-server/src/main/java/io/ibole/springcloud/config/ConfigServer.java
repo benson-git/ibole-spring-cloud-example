4a49e3b1fa590ead963be5fdf21d6c82f06476c6
Benson Wang <chikaiwang@hotmail.com>
2017-03-09 12:49:27 +0800

    add gateway example

--- springcloud-config-server/src/main/java/io/ibole/springcloud/config/ConfigServer.java
+++ springcloud-config-server/src/main/java/io/ibole/springcloud/config/ConfigServer.java
@@ -1,11 +1,15 @@
 package io.ibole.springcloud.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.config.EncryptionAutoConfiguration;
+import org.springframework.cloud.config.server.config.SingleEncryptorAutoConfiguration;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={SingleEncryptorAutoConfiguration.class,EncryptionAutoConfiguration.class})
 public class ConfigServer {
 
 	public static void main(String[] args) {
